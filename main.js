e148be3d040fce06ef523fdc9ea127553dd8c512
caiohenrique6569@gmail.com <caiohenrique6569@gmail.com>
2023-12-20 22:10:59 -0300

    dando funcionalidade ao conversor

---
+++ main.js
@@ -0,0 +1,8 @@
+function calcularDolar() {
+  let real = document.querySelector(".real").value
+  let dolar = document.querySelector(".dolar").value
+
+  let resultado = real / dolar
+
+  alert(resultado)
+}
